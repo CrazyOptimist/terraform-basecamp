21f83ab95579233284b318dcc2d16d10f0af6591
crazyoptimist <crazyoptimist@mail.com>
2021-12-03 15:41:45 -0600

    use data block to query&use existing resources

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = "AKIA4NVWMM7QIOGJQKOG"
-  secret_key = "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"
+  access_key = "AKIA4NVWMM7QA2PQOVMK"
+  secret_key = "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"
 }
 
 resource "aws_vpc" "development-vpc" {
@@ -11,5 +11,15 @@ resource "aws_vpc" "development-vpc" {
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
   cidr_block = "10.0.10.0/24"
+  availability_zone = "us-west-2a"
+}
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.64.0/20"
   availability_zone = "us-west-2a"
 }
