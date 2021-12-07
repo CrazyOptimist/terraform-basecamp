38d70b7b27bbf8b9528306aa352566d17c8ccba2
crazyoptimist <crazyoptimist@mail.com>
2021-12-07 03:21:42 -0600

    remove existing resources

--- main.tf
+++ main.tf
@@ -20,15 +20,3 @@ resource "aws_subnet" "dev-subnet-1" {
   }
 }
 
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-  cidr_block = "172.31.64.0/20"
-  availability_zone = "us-west-2a"
-  tags = {
-    Name = "subnet-2-default"
-  }
-}
