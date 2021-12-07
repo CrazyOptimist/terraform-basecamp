8695177b9c900d394824e7518aae4f6a7db479f8
crazyoptimist <crazyoptimist@mail.com>
2021-12-07 03:26:59 -0600

    add output

--- main.tf
+++ main.tf
@@ -20,3 +20,10 @@ resource "aws_subnet" "dev-subnet-1" {
   }
 }
 
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id" {
+  value = aws_subnet.dev-subnet-1.id
+}
