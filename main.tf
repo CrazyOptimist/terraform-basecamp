80717fb2b1d3690271f621a8124e7e22c6b5cd14
crazyoptimist <crazyoptimist@mail.com>
2021-12-16 20:20:01 -0600

    remove the custom subnet module and its usage

--- main.tf
+++ main.tf
@@ -2,22 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "${var.env_prefix}-vpc"
-  }
-}
-
-module "myapp-subnet" {
-  source = "./modules/subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
-  vpc_id = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-}
-
 module "myapp-server" {
   source = "./modules/webserver"
   vpc_id = aws_vpc.myapp-vpc.id
