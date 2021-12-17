f7f5601e870c9feb099f13008cde5096452bb4f7
crazyoptimist <crazyoptimist@mail.com>
2021-12-16 21:36:23 -0600

    configure terraform backend to use s3

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"
+  backend "s3" {
+    bucket = "q2-terraform-state"
+    key = "myapp/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
