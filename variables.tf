91ee0927cbf0d60011daff02c68367f40348c0de
crazyoptimist <crazyoptimist@mail.com>
2021-12-29 02:27:37 -0600

    refactor variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,22 @@
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
+variable vpc_cidr_block {
+  default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+  default = "10.0.10.0/24"
+}
+variable avail_zone {
+  default = "us-west-2b"
+}
+variable env_prefix {
+  default = "dev"
+}
 variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-variable image_name {}
+variable instance_type {
+  default = "t2.micro"
+}
+variable public_key_location {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable image_name {
+  default = "amzn2-ami-*-x86_64-gp2"
+}
